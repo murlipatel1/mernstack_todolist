88584586cb3bbb1fb93dd6767e2f8c9980d06c13
MURLI PATEL <100035961+murlipatel1@users.noreply.github.com>
2023-01-11 11:39:39 +0530

    Add files via upload

---
+++ backend/middleware/fetchuser.js
@@ -0,0 +1,21 @@
+var jwt = require('jsonwebtoken')
+const JWT_SECRET = "murliisgoody"
+
+const fetchuser = (req , res, next)=>{
+
+    const token =req.header('auth-token')
+    if(!token){
+        res.status(401).send({error:"Please authenticate using valid token"})
+    }
+
+    try {
+        const data = jwt.verify(token , JWT_SECRET)
+    req.user = data.user
+    next()
+    } catch (error) {
+        res.status(401).send({error:"Please authenticate using valid token"})
+    }
+    
+}
+
+module.exports = fetchuser
